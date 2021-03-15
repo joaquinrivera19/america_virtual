9933f4a5581c5dc8aff0af5844164e9bc5809411
Rivera Joaquin <joaquinrivera657@gmail.com>
2021-03-14 23:44:04 -0300

    first commit

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class WeatherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $pais = 'Argentina';
+        $ciudad = 'Ciudad de Buenos Aires';
+
+        $response_actual = Http::get('api.openweathermap.org/data/2.5/weather?q=' . $pais . '&units=metric&lang=sp&appid=626e3e23eedbe0fd9c9cad8a52d6adbb');
+        $weathers_actual = $response_actual->json();
+
+        $resultado = $this->getWeather($pais,$ciudad);
+
+        //dump($weathers_actual);
+        //dd();
+
+        return view('/weather', compact('resultado','pais','ciudad','weathers_actual'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $pais = $request->input('pais');
+        $ciudad = $request->input('ciudad');
+
+        $response_actual = Http::get('api.openweathermap.org/data/2.5/weather?q=' . $pais . '&units=metric&lang=sp&appid=626e3e23eedbe0fd9c9cad8a52d6adbb');
+        $weathers_actual = $response_actual->json();
+    
+        $resultado = $this->getWeather($pais,$ciudad);
+
+        //dump($resultado);
+        //dd();
+
+        return view('/weather', compact('resultado','pais','ciudad','weathers_actual'));
+    }
+
+
+    public function getWeather($pais,$ciudad)
+    {
+        $resultado = [];
+
+        $fechaActual = Carbon::now('America/Argentina/Buenos_Aires')->toDateString();
+        $date = Carbon::parse($fechaActual)->locale('es');
+
+        $response = Http::get('api.openweathermap.org/data/2.5/forecast?q=' . $pais . '&units=metric&lang=sp&appid=626e3e23eedbe0fd9c9cad8a52d6adbb');
+        $weathers = $response->json();
+
+        $dia1 = substr($date->addDays(1), 0, 10);
+        $dia1_nombre = $date->isoFormat('dddd');
+
+        $dia2 = substr($date->addDays(1), 0, 10);
+        $dia2_nombre = $date->isoFormat('dddd');
+
+        $dia3 = substr($date->addDays(1), 0, 10);
+        $dia3_nombre = $date->isoFormat('dddd');
+
+        $dia4 = substr($date->addDays(1), 0, 10);
+        $dia4_nombre = $date->isoFormat('dddd');
+
+        $dia5 = substr($date->addDays(1), 0, 10);
+        $dia5_nombre = $date->isoFormat('dddd');
+
+        foreach ($weathers['list'] as $weather) {
+
+            $rest = substr($weather['dt_txt'], 0, 10);
+
+            if ($rest == $dia1) {
+                $resultado[$rest]['fecha_nombre'] = $dia1_nombre;
+                $resultado[$rest]['fecha'] = $rest;
+                $resultado[$rest]['temp'] = $weather['main']['temp'];
+                $resultado[$rest]['main'] = $weather['weather'][0]['main'];
+                $resultado[$rest]['description'] = $weather['weather'][0]['description'];
+                $resultado[$rest]['icon'] = 'http://openweathermap.org/img/wn/' . $weather['weather'][0]['icon'] . '@2x.png';
+            }
+
+            if ($rest == $dia2) {
+                $resultado[$rest]['fecha_nombre'] = $dia2_nombre;
+                $resultado[$rest]['fecha'] = $rest;
+                $resultado[$rest]['temp'] = $weather['main']['temp'];
+                $resultado[$rest]['main'] = $weather['weather'][0]['main'];
+                $resultado[$rest]['description'] = $weather['weather'][0]['description'];
+                $resultado[$rest]['icon'] = 'http://openweathermap.org/img/wn/' . $weather['weather'][0]['icon'] . '@2x.png';
+            }
+
+            if ($rest == $dia3) {
+                $resultado[$rest]['fecha_nombre'] = $dia3_nombre;
+                $resultado[$rest]['fecha'] = $rest;
+                $resultado[$rest]['temp'] = $weather['main']['temp'];
+                $resultado[$rest]['main'] = $weather['weather'][0]['main'];
+                $resultado[$rest]['description'] = $weather['weather'][0]['description'];
+                $resultado[$rest]['icon'] = 'http://openweathermap.org/img/wn/' . $weather['weather'][0]['icon'] . '@2x.png';
+            }
+
+            if ($rest == $dia4) {
+                $resultado[$rest]['fecha_nombre'] = $dia4_nombre;
+                $resultado[$rest]['fecha'] = $rest;
+                $resultado[$rest]['temp'] = $weather['main']['temp'];
+                $resultado[$rest]['main'] = $weather['weather'][0]['main'];
+                $resultado[$rest]['description'] = $weather['weather'][0]['description'];
+                $resultado[$rest]['icon'] = 'http://openweathermap.org/img/wn/' . $weather['weather'][0]['icon'] . '@2x.png';
+            }
+
+            if ($rest == $dia5) {
+                $resultado[$rest]['fecha_nombre'] = $dia5_nombre;
+                $resultado[$rest]['fecha'] = $rest;
+                $resultado[$rest]['temp'] = $weather['main']['temp'];
+                $resultado[$rest]['main'] = $weather['weather'][0]['main'];
+                $resultado[$rest]['description'] = $weather['weather'][0]['description'];
+                $resultado[$rest]['icon'] = 'http://openweathermap.org/img/wn/' . $weather['weather'][0]['icon'] . '@2x.png';
+            }
+        }
+
+        return $resultado;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/weather.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>AMERICA VIRTUAL</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+    <link href="css/form.css" rel="stylesheet">
+
+    <style>
+        body {
+            background-color: #f2f2f1 !important;
+        }
+
+        .bg-dark {
+            background-color: #ff695a !important;
+        }
+
+        .btn-primary {
+            color: #fff;
+            background-color: #ff695a;
+            border-color: #ff695a;
+        }
+
+        .title_custom {
+            color: #5e5eca !important;
+        }
+
+        .hr_title {
+            border-top: 2px solid rgba(0, 0, 0, .1);
+            background-color: #ff695b;
+        }
+
+        .hr_title_form {
+            border-top: 2px solid rgba(0, 0, 0, .1);
+            background-color: #5e5eca;
+        }
+
+        .sesion {
+            font-size: 14px;
+        }
+
+        .navbar-dark .navbar-nav .nav-link {
+            color: #fff;
+            background-color: #ff695b;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">AMERICA VIRTUAL</a>
+            <button class="btn btn-light nav-link sesion" onclick="openForm()">INICIAR SESIÓN</button>
+
+        </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+
+
+        <div class="row">
+            <div class="col-md-12 mb-5 mt-5 text-center">
+                <h4>SERVICIO DEL CLIMA</h4>
+                <hr class="hr_title">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 mb-5">
+
+                <div class="card">
+                    <div class="card-body">
+
+                        {!! Form::open(array('route' => ['home.store', 'method' => 'POST'])) !!}
+
+                        <h4 class="title_custom">Seleccioná la zona</h4>
+                        <hr class="hr_title_form">
+                        <div class="mb-3">
+                            {!! Form::label('pais', 'País :', ['class' => 'form-label']) !!}
+                            {!! Form::text('pais', null, ['class' => 'form-control','placeholder' => 'Selecciona un pais', 'required' => 'required']) !!}
+                        </div>
+                        <div class="mb-3">
+                            {!! Form::label('ciudad', 'Ciudad :', ['class' => 'form-label']) !!}
+                            {!! Form::text('ciudad', null, ['class' => 'form-control','placeholder' => 'Selecciona una ciudad', 'required' => 'required']) !!}
+                        </div>
+
+                        <button type="submit" class="btn btn-primary mb-3" style="width: 100%;">BUSCAR</button>
+
+                        {!! Form::close() !!}
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-6 mb-5">
+
+
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="title_custom">Reporte</h4>
+                        <hr class="hr_title_form">
+
+                        <div class="row align-items-start">
+                            <div class="col">
+                                <p>
+                                    <small class="text-muted">{{$pais}}</small></br>
+                                    <small class="text-muted">{{$ciudad}}</small>
+                                </p>
+                                <h3>Viernes</h3>
+                                <h4>{{ $weathers_actual['weather'][0]['description'] }}</h4>
+
+                                <h1 style="margin: 25px 0px;">{{ round($weathers_actual['main']['temp']) }} º C</h1>
+                                <h4>{{ $weathers_actual['main']['temp'] }} º F</h4>
+
+                            </div>
+                            <div class="col">
+
+
+
+                                <img src="{{ 'http://openweathermap.org/img/wn/' . $weathers_actual['weather'][0]['icon'] . '@2x.png' }}" class="card-img-top" alt="...">
+                                <p>
+                                    <small class="text-muted">Prob. de precipitaciones: 0 %</small></br>
+                                    <small class="text-muted">Humedad: {{$weathers_actual['main']['humidity']}} %</small></br>
+                                    <small class="text-muted">Viento: a {{$weathers_actual['wind']['speed']}} km/h</small>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <!-- /.row -->
+
+        <div class="row">
+            <div class="card-group mb-5" style="width: 100%;">
+
+                @foreach($resultado as $resul)
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center title_custom">{{ strtoupper($resul['fecha_nombre']) }}</h5>
+                        <img src="{{ $resul['icon'] }}" class="card-img-top" alt="...">
+                        <p class="card-text">{{ round ($resul['temp']) }} º C</p>
+                        <p class="card-text">{{ $resul['temp'] }} º F</p>
+                    </div>
+                </div>
+
+                @endforeach
+
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer style="background-color: #343a40!important; padding: 15px 0px;">
+        <div class="container">
+            <p class="m-0 text-left text-white">Copyright 2021 All right register America Virtual</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+
+
+    <div class="form-popup" id="myForm">
+        <form action="/action_page.php" class="form-container">
+            <label>Usuario</label>
+            <input type="text" placeholder="americavirtual@gmail.com" name="usuario" required>
+
+            <label>Contraseña</label>
+            <input type="password" placeholder="" name="password" required>
+
+            <button type="submit" class="btn">INICIAR SESIÓN</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Cerrar</button>
+        </form>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        function openForm() {
+            document.getElementById("myForm").style.display = "block";
+        }
+
+        function closeForm() {
+            document.getElementById("myForm").style.display = "none";
+        }
+    </script>
+
+</body>
+
+</html>
